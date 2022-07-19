26dadd9b2baf9a0f176f72a6a282420794723005
Роман <romamorozevich@yandex.ru>
2022-07-19 18:16:10 +0300

    final commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from pprint import pprint
+from tqdm import tqdm
 
 
 class VK:
@@ -12,10 +13,10 @@ class VK:
 
     def get_photos(self):
         url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id': self.id, 'extended': 1, 'album_id': 'profile'}
+        params = {'owner_id': self.id, 'extended': 1, 'album_id': 'profile', 'count':5}
         response = requests.get(url, params={**self.params, **params})
         return response.json()
-photo_url = 'https://sun9-88.userapi.com/c9591/u00001/136592355/w_818d6f79.jpg'
+
 
 class YaUpLoader:
     def __init__(self, token: str):
@@ -29,18 +30,20 @@ class YaUpLoader:
     def upload(self, file_path, photo_url):
         url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {'path': file_path, 'url1' : photo_url}
+        params = {'path': file_path, 'url1': photo_url, 'overwrite': 'true'}
+
         response = requests.get(url, headers=headers, params=params)
         return response.json()
 
     def upload_to_disk(self, file_path, photo_url):
         response_href = self.upload(file_path, photo_url)
         href = response_href.get('href', '')
-        response_ = requests.put(href, data=photo_url)
+        requests.put(href, data=open(photo_url, 'rb'))
 
-        response_.raise_for_status()
-        if response_.status_code == 201:
-            print('Success')
+    def create_folder(self, path):
+        URL = 'https://cloud-api.yandex.net/v1/disk/resources'
+        headers = self.get_headers()
+        requests.put(f'{URL}?path={path}', headers=headers)
 
 
 with open('token.txt', 'r') as file_object:
@@ -48,68 +51,105 @@ with open('token.txt', 'r') as file_object:
 user_id = input('Введите id пользователя')
 vk = VK(access_token, user_id)
 
-with open('yandex_token.txt', 'r') as file_object:
-    yandex_token = file_object.read().strip()
+yandex_token = input('Введите яндекс токен: ')
 ya = YaUpLoader(yandex_token)
-print(yandex_token)
 
-dict_ = {}
 
+url_dict = {}
+likes_ = []
+size_and_like_dict = {}
+final_file = []
 photos = vk.get_photos()
+repeatable_likes = []
+
+
+for lala in photos['response']['items']:
+    likes_.append(lala['likes']['count'])
+for like in likes_:
+    if likes_.count(like) != 1:
+        repeatable_likes.append(str(like))
 
 
 def get_photo_url():
     for info in photos['response']['items']:
         base = 0
+        date = str(info['date'])
         big_photo = ''
-        likes = info['likes']['count']
+        size_ = ''
+        likes = str(info['likes']['count'])
+
         for size in info['sizes']:
             if size['type'] == 'w':
                 big_photo = size['url']
                 base = 1
+                size_ = size['type']
 
             elif size['type'] == 'z' and base != 1:
                 big_photo = size['url']
                 base = 2
+                size_ = size['type']
 
             elif size['type'] == 'y' and base != 1 or 2:
                 big_photo = size['url']
                 base = 3
+                size_ = size['type']
 
             elif size['type'] == 'r' and base != 1 or 2 or 3:
                 big_photo = size['url']
                 base = 4
+                size_ = size['type']
             elif size['type'] == 'q' and base != 1 or 2 or 3 or 4:
                 big_photo = size['url']
                 base = 5
+                size_ = size['type']
 
             elif size['type'] == 'p' and base != 1 or 2 or 3 or 4 or 5:
                 big_photo = size['url']
                 base = 6
+                size_ = size['type']
             elif size['type'] == 'o' and base != 1 or 2 or 3 or 4 or 5 or 6:
                 big_photo = size['url']
                 base = 7
+                size_ = size['type']
             elif size['type'] == 'x' and base != 1 or 2 or 3 or 4 or 5 or 6 or 7:
                 big_photo = size['url']
                 base = 8
+                size_ = size['type']
             elif size['type'] == 'm' and base != 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8:
                 big_photo = size['url']
                 base = 9
+                size_ = size['type']
             elif size['type'] == 's' and base != 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
                 big_photo = size['url']
+                size_ = size['type']
 
-        dict_[likes] = big_photo
+        size_and_like_dict['filename'] = likes + 'jpg'
+        size_and_like_dict['size'] = size_
+
+        final_file.append(size_and_like_dict)
+
+        if likes in repeatable_likes:
+            url_dict[likes + '_' + date] = big_photo
+
+        else:
+            url_dict[likes] = big_photo
 
 
-# pprint(photos)
+path = 'course_project_photos'
+ya.create_folder(path)
 get_photo_url()
 
-pprint(dict_)
-for photo in dict_:
-    likes = str(photo)
-    photo_url = dict_[photo]
-    ya.upload_to_disk(file_path = 'photos/' + likes + '.jpg',photo_url = photo_url)
-# print(photo_url)
+pprint(final_file)
+images = []
 
+for filename in url_dict:
+    image = url_dict[filename]
+    api = requests.get(image)
 
-# ya.upload_to_disk()
+    with open(f'photos/{filename}.jpg', 'wb') as file:
+        file.write(api.content)
+        images.append(filename + '.jpg')
+
+for image_ in tqdm(images):
+    file_path = f'photos\\{image_}'
+    ya.upload_to_disk(file_path=f'{path}/' + image_, photo_url=file_path)
